0d68b924d124d341d3b68a125570e02dc627c254
Raine <64136899+dawnvt@users.noreply.github.com>
2021-09-02 21:18:46 +0200

    Add files via upload

---
+++ AccSaber/Installers/MenuInstaller.cs
@@ -0,0 +1,21 @@
+﻿using AccSaber.UI.Leaderboard;
+using AccSaber.UI.Leaderboard.Panel;
+using SiraUtil;
+using Zenject;
+
+namespace AccSaber.Installers
+{
+    public class MenuInstaller : Installer
+    {
+        public MenuInstaller()
+        {
+        }
+        
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<AccSaberLeaderboardViewController>().FromNewComponentAsViewController().AsSingle();
+            Container.BindInterfacesAndSelfTo<AccSaberPanelController>().FromNewComponentAsViewController().AsSingle();
+            Container.BindInterfacesAndSelfTo<LeaderboardUI>().AsSingle();
+        }
+    }
+}
