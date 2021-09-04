80f91849a518a9e98f5d1a5d2e60d420e7e94a5b
Raine <64136899+dawnvt@users.noreply.github.com>
2021-09-04 02:54:46 +0200

    change for leaderboardcore registering change

--- AccSaber/Installers/MenuInstaller.cs
+++ AccSaber/Installers/MenuInstaller.cs
@@ -1,21 +1,25 @@
 ﻿using AccSaber.UI.Leaderboard;
 using AccSaber.UI.Leaderboard.Panel;
+using AccSaber.Managers;
 using SiraUtil;
+using SiraUtil.Tools;
 using Zenject;
 
 namespace AccSaber.Installers
 {
     public class MenuInstaller : Installer
     {
-        public MenuInstaller()
+        private readonly SiraLog _log;
+        public MenuInstaller(SiraLog log)
         {
+            _log = log;
         }
         
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<AccSaberLeaderboardViewController>().FromNewComponentAsViewController().AsSingle();
             Container.BindInterfacesAndSelfTo<AccSaberPanelController>().FromNewComponentAsViewController().AsSingle();
-            Container.BindInterfacesAndSelfTo<LeaderboardUI>().AsSingle();
+            Container.BindInterfacesAndSelfTo<AccSaberManager>().AsSingle().NonLazy();
         }
     }
 }
